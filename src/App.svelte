3f8c9c313be82f203a334650bce8e9683f1fdc2b
sjc <email@sjc.sh>
2021-09-27 19:35:21 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import MaterialButton from "./MaterialButton.svelte";
+</script>
+
+<MaterialButton
+  id="one"
+  text="first button"
+  onClickFunction={() => {
+    console.log("you just clicked the first button");
+  }}
+/>
+<MaterialButton
+  id="two"
+  text="second button"
+  onClickFunction={() => {
+    console.log("you just clicked the second button");
+  }}
+/>
+
+<style global lang="scss">
+  @use "@material/button/styles";
+</style>
---
+++ src/MaterialButton.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+  import { MDCRipple } from "@material/ripple";
+
+  onMount(() => {
+    const buttonRipple = new MDCRipple(document.getElementById(id));
+  });
+
+  export let id;
+  export let text;
+  export let onClickFunction;
+</script>
+
+<button class="mdc-button" {id} on:click={onClickFunction}>
+  <span class="mdc-button__ripple" />
+  <span class="mdc-button__label">{text}</span>
+</button>
